f4c5f92c0de4db8d81c3a0a939d03d828bc1bf8e
yanovskyi <pavlo.yanovsky@gmail.com>
2023-06-23 14:32:01 +0300

    add dino running animation

--- src/app/game-wrapper/game-wrapper.component.ts
+++ src/app/game-wrapper/game-wrapper.component.ts
@@ -1,37 +1,63 @@
+/* angular */
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 
+
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+const MAIN_SPRITE_SRC = './assets/img/dino-sprite.png';
+const DINO_FRAME_WIDTH = 88;
+const DINO_FRAME_HEIGHT = 90;
+const DINO_X_START_POSITION = 1678;
+const DINO_Y_START_POSITION = 5;
+const DINO_FRAME_NUMBER = 4;
 @Component({
   selector: 'app-game-wrapper',
   templateUrl: './game-wrapper.component.html',
-  styleUrls: ['./game-wrapper.component.scss']
+  styleUrls: ['./game-wrapper.component.scss'],
 })
 export class GameWrapperComponent implements AfterViewInit {
   @ViewChild('gameArea') public gameArea: any;
 
-  constructor() {}
+  private gameCanvas!: HTMLCanvasElement;
+  private gameCtx!: CanvasRenderingContext2D;
+  private dinoImg: HTMLImageElement;
+  private gameFrame = 0;
+  private staggerFrames = 5;
 
-  ngAfterViewInit() {
-    const gameCanvas = this.gameArea.nativeElement as HTMLCanvasElement;
-    gameCanvas.width = 800;
-    gameCanvas.height = 800;
-    if (gameCanvas) {
-      const gameCanvasContext: CanvasRenderingContext2D = gameCanvas.getContext('2d') as CanvasRenderingContext2D;
-      if (gameCanvasContext) {
-        const dinoImg = new Image();
-        dinoImg.src = './assets/img/dino-sprite.png';
-        gameCanvasContext.beginPath();
-        gameCanvasContext.fillStyle = 'red';
-        gameCanvasContext.arc(10, 10, 10, 0, Math.PI * 2);
-        dinoImg.onload = () => {
-          gameCanvasContext.drawImage(dinoImg, 100, 100, 200, 200);
-        }
-        gameCanvasContext.fill();
-        gameCanvasContext.fillStyle = 'pink';
-
-        gameCanvasContext.fillRect(20, 20, 150, 100);
-
-      }
-    }
+  constructor() {
+    this.dinoImg = new Image();
+    this.dinoImg.src = MAIN_SPRITE_SRC;
   }
 
+  ngAfterViewInit() {
+    this.gameCanvas = this.gameArea.nativeElement as HTMLCanvasElement;
+    this.gameCanvas.width = CANVAS_WIDTH;
+    this.gameCanvas.height = CANVAS_HEIGHT;
+    this.gameCtx = this.gameCanvas.getContext('2d') as CanvasRenderingContext2D;
+    this.animate();
+  }
+  animate() {
+    this.gameCtx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    const dinoPosition = Math.floor(this.gameFrame / this.staggerFrames) % (DINO_FRAME_NUMBER - 1);
+    const dinoFrameX = (dinoPosition + 1) * DINO_FRAME_WIDTH + DINO_X_START_POSITION;
+    const dinoFrameY = DINO_Y_START_POSITION;
+    const dinoCanvasXposition = 0;
+    const dinoCanvasYposition = CANVAS_HEIGHT - 400;
+    const dinoCanvasWidth = 150;
+    const dinoCanvasHeight = 150;
+    this.gameCtx.drawImage(
+      this.dinoImg,
+      dinoFrameX,
+      dinoFrameY,
+      DINO_FRAME_WIDTH,
+      DINO_FRAME_HEIGHT,
+      dinoCanvasXposition,
+      dinoCanvasYposition,
+      dinoCanvasWidth,
+      dinoCanvasHeight
+    );
+
+    this.gameFrame++;
+    requestAnimationFrame(() => this.animate());
+  }
 }
